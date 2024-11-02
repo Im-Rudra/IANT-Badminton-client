ab032b533c3bb209f50400bbcbc4215c3360ccc5
Im-Rudra <imrudra.me@gmail.com>
2024-11-02 20:27:59 +0600

    refactor: tournament card refactored

--- src/components/TournamentCard.jsx
+++ src/components/TournamentCard.jsx
@@ -5,8 +5,15 @@ import { IoPeople } from 'react-icons/io5';
 import { HiUser } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 
-const TournamentCard = ({ tournament, buttonTitle, link }) => {
-  // const { tournamentName, status, totalTeams, singleTeams, doubleTeams } = tournament;
+const TournamentCard = ({ tournament, buttonTitle, link, handlers, isAdmin = false }) => {
+
+  const toggleTournamentStatus = (id, tournament) => {
+    const confirm = window.confirm(`Do you want to ${tournament.status === "Open" ? "close" : "reopen"} the tournament?`);
+    if (!confirm) return;
+    const status = tournament.status === "Open" ? "Closed" : "Open";
+    handlers.handleCloseTournament(id, status);
+  };
+
   return (
     <Card
       style={{
@@ -22,6 +29,17 @@ const TournamentCard = ({ tournament, buttonTitle, link }) => {
           <Link to={link}>
             <Button type="primary">{buttonTitle}</Button>
           </Link>
+          {isAdmin && (
+            <Button
+              className={`ml-2 hover:opacity-80 ${
+                tournament.status === 'Open' ? '!bg-red-600/80' : '!bg-green-500/80'
+              }`}
+              type="primary"
+              onClick={() => toggleTournamentStatus(tournament._id, tournament)}
+            >
+              {tournament.status === 'Open' ? 'Close' : 'Open'}
+            </Button>
+          )}
         </>
       }
     >
